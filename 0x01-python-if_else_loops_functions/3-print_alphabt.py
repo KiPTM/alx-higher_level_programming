18e33f892ee4c4b359dd07604db02c0684de1269
KiPTM <mathewtuwei@gmail.com>
2023-11-28 05:47:55 -0800

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in ['q', 'e']:
+        print(chr(char), end='')
