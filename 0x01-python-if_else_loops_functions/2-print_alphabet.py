118016f377ff0fa942357503ea105bd6ec49fbaa
KiPTM <mathewtuwei@gmail.com>
2023-11-28 05:15:52 -0800

    ASCII

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,4 @@
+for i in range(97, 123):
+    print(chr(i), end='')
+
+# Output: abcdefghijklmnopqrstuvwxyz
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+for i in range(97, 123):
+    if chr(i) not in ['q', 'e']:
+        print(chr(i), end='')
+
+# Output: abcdfghijklmnoprstuvwxyz
