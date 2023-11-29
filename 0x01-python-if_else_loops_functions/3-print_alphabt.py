2378ab1e0636f6d9bb35f6aaa93676db09c094f5
KiPTM <mathewtuwei@gmail.com>
2023-11-29 00:41:29 -0800

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    if chr(char) not in ['q', 'e']:
+for i in range(97, 123):
+    if (i == 101) or (i == 113):
         continue
-    print(chr(i).format(), end='')
+    print(chr(i).format(), end="")
