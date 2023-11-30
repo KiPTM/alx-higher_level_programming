44872d2ebf70cbf04d27f680e71d9f756ab59bf1
KiPTM <mathewtuwei@gmail.com>
2023-11-30 04:55:33 -0800

    import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-
-# Define a function that imports add_0.py and performs the addition
-def import_and_add():
+if __name__ == "__main__":
     from add_0 import add
-    result = add(a, b)
-    return result
 
-# Call the function and print the result using string formatting
-result = import_and_add()
-print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b))
