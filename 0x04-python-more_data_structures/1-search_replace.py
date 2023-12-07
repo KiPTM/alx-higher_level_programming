f50d6f18d0c9da9638db01f5259b82b6a63f8563
KiPTM <mathewtuwei@gmail.com>
2023-12-07 00:05:20 -0800

    idhbcierbu

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    # Create a new list to store the replaced elements
-    new_list = []
-
-    # Iterate through the original list
-    for item in my_list:
-        # Replace 'search' with 'replace' if found, otherwise keep the original item
-        if item == search:
-            new_list.append(replace)
-        else:
-            new_list.append(item)
-
-    return new_list
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
