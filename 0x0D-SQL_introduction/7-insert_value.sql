9b21576da97db368b67f5557e151efea58785a6e
KiPTM <mathewtuwei@gmail.com>
2024-02-13 12:53:51 -0800

    Update

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
---This coode inserts a ROW into the table first_table
-INSERT INTO 'first_table' ('id' INT, 'name' VARCHAR) VALUES (89, "Best School");
+-- Inserts a new row into the table first_table in my MySQL server.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
