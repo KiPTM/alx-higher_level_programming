f7c1bcf660afc597b51dba03daaf5a24a4095951
KiPTM <mathewtuwei@gmail.com>
2024-02-13 11:16:16 -0800

    SQL basics

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 --This coode inserts a ROW into the table first_table
 
-INSERT INTO 'first_table' ('id' INT, 'name' VARCHAR) VALUES (98, 'Best School'
+INSERT INTO 'first_table' ('id' INT, 'name' VARCHAR) VALUES (98, "Best School");
