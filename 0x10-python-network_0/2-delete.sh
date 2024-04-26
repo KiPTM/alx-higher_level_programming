c7dc44821020507156690de0d3a126c29fc351c7
KiPTM <mathewtuwei@gmail.com>
2024-04-26 05:09:17 -0700

    Python Network

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # This script sends a DELETE request to the URL passed as the first argument and displays the body of the response
-
-# Check if a URL is provided as argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send DELETE request to the provided URL and display the body of the response
 curl -X DELETE -s "$1"
