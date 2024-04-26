c133e6f89cc9a064ecb7bb70405a9fe6a366c1eb
KiPTM <mathewtuwei@gmail.com>
2024-04-26 04:48:21 -0700

    Python Network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL using curl, and displays a specific message based on the response
+curl -s "$1" | grep -o "my index page\|my index page is so cool! Mainly because it’s in Python" || echo "Unknown response"
 
-response=$(curl -s "$1")
-
-if [[ $response == *"my index page"* ]]; then
-    echo "GET / => \"my index page\""
-elif [[ $response == *"my index page is so cool! Mainly because it’s in Python"* ]]; then
-    echo "GET / => \"my index page is so cool! Mainly because it’s in Python\""
-else
-    echo "Unknown response"
-fi
