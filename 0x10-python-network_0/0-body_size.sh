e14295811d026b1fe90e229ab83dbeb18ee65773
KiPTM <mathewtuwei@gmail.com>
2024-04-26 04:44:30 -0700

    Python Network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
+# This script takes a URL as an argument, sends a request to that URL using curl, and displays the size of the body of the response in bytes
 
-# Check if a URL is provided as argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request to the provided URL and display the size of the body of the response in bytes
 curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
