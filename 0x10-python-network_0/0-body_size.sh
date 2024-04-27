22706b202156e8c541c2b13b9c7a671224ce60ef
KiPTM <mathewtuwei@gmail.com>
2024-04-26 22:28:46 -0700

    Update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a request to that URL using curl, and displays a specific message based on the response
-curl -s "$1" | grep -o "my index page\|my index page is so cool! Mainly because it’s in Python" || echo "Unknown response"
+# This script sends a request to the URL and displays the size of the body of the response in bytes
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
