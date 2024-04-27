602188b04749279c336ca8f47893d2e78713ac30
KiPTM <mathewtuwei@gmail.com>
2024-04-27 00:23:18 -0700

    Update

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Fetches https://alx-intranet.hbtn.io/status using the requests package and displays the body of the response.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    
+    print("Body response:")
+    print("\t- type:", type(response.text))
+    print("\t- content:", response.text)
