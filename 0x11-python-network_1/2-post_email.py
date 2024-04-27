cabf69d7ce78f40dcce7ac9e5b7af60a739527f4
KiPTM <mathewtuwei@gmail.com>
2024-04-27 00:17:24 -0700

    Update

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,14 +4,14 @@ Sends a POST request to the passed URL with the email as a parameter and display
 """
 
 import urllib.request
-import urllib.parse
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
 
-    data = urllib.parse.urlencode({'email': email}).encode()
+    data = "email=" + email
+    data = data.encode('utf-8')
     req = urllib.request.Request(url, data=data, method='POST')
 
     with urllib.request.urlopen(req) as response:
