77893e08e35485a24057f1dab11b92c747ef193b
KiPTM <mathewtuwei@gmail.com>
2024-04-26 22:39:10 -0700

    Update

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,18 @@
+#!/usr/bin/python3
+""" Test function find_peak """
 def find_peak(list_of_integers):
     if not list_of_integers:
         return None
-    
-    low, high = 0, len(list_of_integers) - 1
-    
-    while low < high:
-        mid = (low + high) // 2
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid
-        else:
-            low = mid + 1
-    
-    return list_of_integers[low]
+
+    peak = list_of_integers[0]
+    for num in list_of_integers:
+        if num > peak:
+            peak = num
+
+    return peak
 
 # Test cases
-print(find_peak([1, 2, 3, 4, 5]))  # Output: 5
-print(find_peak([5, 4, 3, 2, 1]))  # Output: 5
-print(find_peak([1, 4, 6, 2, 1]))  # Output: 6
-print(find_peak([5, 4, 6, 2, 1, 4, 5, 2]))  # Output: 6
+print(find_peak([1, 2, 3, 4, 5]))
+print(find_peak([5, 4, 3, 2, 1]))
+print(find_peak([1, 4, 6, 2, 1]))
+print(find_peak([5, 4, 6, 2, 1, 4, 5, 2]))
