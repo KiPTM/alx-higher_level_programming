7cfb22245f226514635acb80494fb16fefb37911
KiPTM <mathewtuwei@gmail.com>
2024-06-20 09:36:15 -0700

    update

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to the URL and displays the body of the response for a 200 status code
-curl -s -o /dev/null -w "%{http_code}" "$1" | { read code; [ $code -eq 200 ] && curl -s "$1"; }
+curl -s -L "${1}"
