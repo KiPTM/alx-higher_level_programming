92860631523506cecbeeb4844bc552f12f0b5568
KiPTM <mathewtuwei@gmail.com>
2024-06-20 09:47:19 -0700

    update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes a URL as an argument and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep -i Allow | awk '{print $2}'
+curl -s -I "${1}" | grep "^Allow: .*" | cut -d " " -f 2-
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-""" Test function find_peak """
+"""Algorithms for list of integers."""
+
+
 def find_peak(list_of_integers):
-    if not list_of_integers:
-        return None
-
-    peak = list_of_integers[0]
-    for num in list_of_integers:
-        if num > peak:
-            peak = num
-
-    return peak
-
-# Test cases
-print(find_peak([1, 2, 3, 4, 5]))
-print(find_peak([5, 4, 3, 2, 1]))
-print(find_peak([1, 4, 6, 2, 1]))
-print(find_peak([5, 4, 6, 2, 1, 4, 5, 2]))
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
